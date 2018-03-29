ae508c4b8b299c5acccc0a06c77047e6a16907a0
Training_C2a.05.01 <Training_C2a.05.01@accenture.com>
2018-03-29 14:15:48 +0530

    Java Code

---
+++ JenkinsLearning.java
@@ -0,0 +1,12 @@
+package jenkinsLearning;
+
+public class JenkinsLearning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Learning DevOps Testing");
+
+	}
+
+}
